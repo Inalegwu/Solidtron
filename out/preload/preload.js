6eb4c4a00363f59884088cfe6902085bdfac4719
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-05-26 13:56:39 +0100

    removed out

--- out/preload/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const main = require("electron-trpc/main");
-console.log("fuck yeah i'm mounted up...");
-process.once("loaded", () => {
-  main.exposeElectronTRPC();
-});
--- src/shared/storage.ts
+++ src/shared/storage.ts
@@ -6,6 +6,9 @@ const store = createStore().setTablesSchema({
     name: {
       type: "string",
     },
+    phoneNumber: {
+      type: "number",
+    },
   },
 });
 
